d25e2e48f521def1aa4e1080f1f69c919c989f44
Logan <logan.manlief@gmail.com>
2023-12-11 17:04:54 -0500

    Pages Structure

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const path = require('path');
+
 
 app.get('/', (req, res) => {
-    res.send("Hello World");
+    res.sendFile(path.join(__dirname, '/content/pages/index.html'));
 });
 
 app.listen(port, () => {
